80fe803d066ea02b11a3eefdd0eef81b9e31126c
Karl Johannes Jondell <kj@jondell.com>
2021-03-04 09:41:53 +0100

    nytt frontend med react

--- python/app.py
+++ python/app.py
@@ -11,28 +11,42 @@ app = Flask(__name__)
 app.secret_key = os.urandom(42)
 dataSender = DataSender()
 
-@app.route('/')
-def load_start_page():
-    return render_template('index.html', title="Diabetes sonifiering")
-
-@app.route('/uploader', methods=['POST'])
-def upload_new_file():
-    if request.method == 'POST':
+# @app.route('/')
+# def load_start_page():
+#     return render_template('index.html', title="Diabetes sonifiering")
+# 
+# @app.route('/uploader', methods=['POST'])
+# def upload_new_file():
+#     if request.method == 'POST':
+#         f = request.files['file']
+# 
+#         thread = Thread(target=dataSender.send_file, args=(f,))
+#         thread.daemon = True
+#         thread.start()
+# 
+#         return redirect(url_for('listen'))
+# 
+# @app.route('/listen')
+# def listen():
+#     return render_template('listen.html', title="Lyssna")
+# 
+# @app.route('/upload')
+# def upload_file():
+#     return render_template('upload.html', title="Uppladdning")
+# 
+@app.route('/api/uppladdning', methods=['POST'])
+def upload():
+    try:
         f = request.files['file']
 
         thread = Thread(target=dataSender.send_file, args=(f,))
         thread.daemon = True
         thread.start()
 
-        return redirect(url_for('listen'))
-
-@app.route('/listen')
-def listen():
-    return render_template('listen.html', title="Lyssna")
-
-@app.route('/upload')
-def upload_file():
-    return render_template('upload.html', title="Uppladdning")
+        return {'uploadSuccess' : True}
+    except:
+        return {'uploadSuccess' : False}
 
 if __name__ == '__main__':
     app.run(debug = True)
+ 
