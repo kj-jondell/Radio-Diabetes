89225903a03023ad32451fcaf93443a2060921c6
Karl Johannes <kj@jondell.com>
2021-05-07 20:01:12 +0200

    added gunicorn config

--- python/app.py
+++ python/app.py
@@ -7,14 +7,22 @@
 from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from DataSender import DataSender 
+import sendgrid
+from sendgrid.helpers.mail import *
+
 import datetime
 
 from threading import Thread
 from pythonosc import udp_client
 
-import os
+import os, sys
 
 app = Flask(__name__)
+
+sg = sendgrid.SendGridAPIClient(api_key=SENDGRID_API_KEY)
+from_email = Email("info@radiodiabetes.eu")
+to_email = Email("kj@jondell.com")
+subjekt = "Nytt bidrag!"
 
 app.secret_key = os.urandom(42)
 app.debug = True
@@ -34,8 +42,16 @@ def upload():
         if len(message)>0:
             with open(f"../../messages/{datetime.datetime.now().strftime('%Y%m%d-%H_%M_%S')}.txt", "w") as message_file:
                 message_file.write(message)
+                content = Content("text/plain", message)
+                mail = Mail(from_email, to_email, subjekt, content)
+                response = sg.client.mail.send(post(request_body=mail.get()))
+                print(response.status_code)
+                print(response.body)
+                print(response.headers)
 
         return {'uploadSuccess' : True}
+    except ConnectionRefusedError as err:
+        print(err)
     except:
         return {'uploadSuccess' : False}
 
---
+++ python/gunicorn_config.py
@@ -0,0 +1,12 @@
+bind = "0.0.0.0:4000"
+workers = 1 #TODO flera workers?
+# SENDGRID_API_KEY = "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"
+
+# MAIL_SERVER = "smtp.gmail.com"
+# MAIL_PORT = 465
+# MAIL_USE_TLS = False
+# MAIL_USE_SSL = True
+# MAIL_DEFAULT_SENDER = "kj@jondell.com"
+# MAIL_USERNAME = "kj@jondell.com"
+# MAIL_PASSWORD = "Apelsin8!"
+
